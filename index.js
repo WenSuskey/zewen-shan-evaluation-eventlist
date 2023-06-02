fb9a94e878897ebfa2f42dd92203bbe911dca8a0
zack129 <zewen.shan@authright.com>
2023-06-02 18:26:56 -0400

    comple add

--- index.js
+++ index.js
@@ -72,8 +72,8 @@ class EventsModel{
     this.#events.filter(el=>el.id!==id)
     return event
     }
-    async putTodo(id,newEvent){
-        const todo = await API.putTodo(id,newEvent)
+    async updateEvent(id,newEvent){
+        const todo = await API.updateEvent(id,newEvent)
         return todo
    }
 }
@@ -122,6 +122,32 @@ class EventsView{
         console.log({title,startDate,endDate})
         return {title,startDate,endDate}
     }
+    updateRender(newEvent){
+        const event = this.createEvent(newEvent)
+        const element = document.getElementById(`event-${newEvent.id}`)
+        element.innerHTML = `
+        <div class="title" id="event-${newEvent.id}">
+            ${newEvent.eventName}
+        </div>
+        <div class="start-date" id="start-${newEvent.id}">
+            ${newEvent.startDate}
+        </div>
+        <div class="end-date" id="end-${newEvent.id}">
+            ${newEvent.endDate}
+        </div>
+        <div class="event-actions">
+            <button class="edit-btn" edit-id=${newEvent.id}>
+                <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>
+            </button>
+            <button class="delete-btn" delete-id=${newEvent.id}>
+                <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>
+            </button>
+        
+        </div>
+        `
+        console.log(this.eventsList)
+
+    }
     deleteAdd(){
         const element = document.getElementById(`add-event`)
         element.remove()
@@ -135,8 +161,9 @@ class EventsView{
 
     updateEventHandeler(id){
         const element = document.getElementById(`event-${id}`)
+        
         element.innerHTML= `
-        <div class="event" id="event-${id}">
+
             <input type = "text" id="update-title-${id}"/>
             <input type = "date" id="update-strat-${id}"/> 
             <input type = "date" id="update-end-${id}"/> 
@@ -144,27 +171,27 @@ class EventsView{
                 <button class = "update-save" update-save-id="${id}">add</button>
                 <button id="update-cancel">cancel</button>
             </div>
-        </div>`
+        `
     }
 
     //create event data
     createEvent(event){
         //tr- contains the event
-        const eventElement = document.createElement('tr')
+        const eventElement = document.createElement('div')
         eventElement.classList.add('event')
         eventElement.setAttribute("id",`event-${event.id}`)
         //event Title
-        const eventTitle = document.createElement('th')
+        const eventTitle = document.createElement('div')
         eventTitle.classList.add('title')
         eventTitle.setAttribute("id",`title-${event.id}`)
         eventTitle.textContent = event.eventName
         //start date
-        const startDate = document.createElement('th')
+        const startDate = document.createElement('div')
         startDate.classList.add('start-date')
         startDate.setAttribute("id",`start-${event.id}`)
         startDate.textContent = event.startDate
         //end date
-        const endDate = document.createElement('th')
+        const endDate = document.createElement('div')
         endDate.classList.add('end-date')
         endDate.setAttribute("id",`end-${event.id}`)
         endDate.textContent = event.endDate
@@ -215,35 +242,35 @@ class EventsView{
         createElement.append(eventTitle,startDate,endDate,actions)
         return createElement
     }
-    // createUpate(id){
-    //     //tr- contains the event
-    //     const createElement = document.createElement('div')
-    //     createElement.classList.add('add-event')
-    //     createElement.setAttribute("id",`add-event`)
-    //     //event Title
-    //     const eventTitle = document.createElement('input')
-    //     eventTitle.setAttribute("type",`text`)
-    //     eventTitle.setAttribute("id",`input-title-${id}`)
-    //     const startDate = document.createElement('input')
-    //     startDate.setAttribute("type",`date`)
-    //     startDate.setAttribute("id",`add-startDate-${id}`)
-    //     const endDate = document.createElement('input')
-    //     endDate.setAttribute("type",`date`)
-    //     endDate.setAttribute("id",`add-endDate-${id}`)    
-    //     const actions = document.createElement('div')
-    //     actions.classList.add('event-actions')
-    //     const saveBtn = document.createElement('button')
-    //     saveBtn.classList.add('save-btn')
-    //     saveBtn.setAttribute("saveBtn-id",`${id}`)
-    //     saveBtn.innerHTML= '<svg focusable viewBox="0 0 24 24" aria-hidden="true xmlns="http://www.w3.org/2000/svg"><path d="M12 6V18M18 12H6" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>'
-    //     const cancelBtn = document.createElement('button')
-    //     cancelBtn.classList.add('cancel-btn')
-    //     cancelBtn.setAttribute("id",`cancleBtn-${id}`)
-    //     cancelBtn.innerHTML= '<svg focusable="false" aria-hidden="true" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z"></path></svg>'
-    //     actions.append(saveBtn,cancelBtn)
-    //     createElement.append(eventTitle,startDate,endDate,actions)
-    //     return createElement
-    // }
+    createUpdateEvent(event){
+        //tr- contains the event
+        const createElement = document.createElement('div')
+        createElement.classList.add('add-event')
+        createElement.setAttribute("id",`add-event`)
+        //event Title
+        const eventTitle = document.createElement('input')
+        eventTitle.setAttribute("type",`text`)
+        eventTitle.setAttribute("id",`input-title-${id}`)
+        const startDate = document.createElement('input')
+        startDate.setAttribute("type",`date`)
+        startDate.setAttribute("id",`add-startDate-${id}`)
+        const endDate = document.createElement('input')
+        endDate.setAttribute("type",`date`)
+        endDate.setAttribute("id",`add-endDate-${id}`)    
+        const actions = document.createElement('div')
+        actions.classList.add('event-actions')
+        const saveBtn = document.createElement('button')
+        saveBtn.classList.add('save-btn')
+        saveBtn.setAttribute("saveBtn-id",`${id}`)
+        saveBtn.innerHTML= '<svg focusable viewBox="0 0 24 24" aria-hidden="true xmlns="http://www.w3.org/2000/svg"><path d="M12 6V18M18 12H6" stroke="#FFFFFF" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+        const cancelBtn = document.createElement('button')
+        cancelBtn.classList.add('cancel-btn')
+        cancelBtn.setAttribute("id",`cancleBtn-${id}`)
+        cancelBtn.innerHTML= '<svg focusable="false" aria-hidden="true" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><path d="M19.587 16.001l6.096 6.096c0.396 0.396 0.396 1.039 0 1.435l-2.151 2.151c-0.396 0.396-1.038 0.396-1.435 0l-6.097-6.096-6.097 6.096c-0.396 0.396-1.038 0.396-1.434 0l-2.152-2.151c-0.396-0.396-0.396-1.038 0-1.435l6.097-6.096-6.097-6.097c-0.396-0.396-0.396-1.039 0-1.435l2.153-2.151c0.396-0.396 1.038-0.396 1.434 0l6.096 6.097 6.097-6.097c0.396-0.396 1.038-0.396 1.435 0l2.151 2.152c0.396 0.396 0.396 1.038 0 1.435l-6.096 6.096z"></path></svg>'
+        actions.append(saveBtn,cancelBtn)
+        createElement.append(eventTitle,startDate,endDate,actions)
+        return createElement
+    }
 
    
      
@@ -309,13 +336,18 @@ class EventsController{
         this.view.eventsList.addEventListener("click",(e)=>{
             if(e.target.classList.contains('edit-btn')){
                 const updateId = e.target.getAttribute("edit-id")
-                console.log(updateId)
                 this.view.updateEventHandeler(updateId)
             }
             else if(e.target.classList.contains('update-save')){
                 const updateId = e.target.getAttribute("update-save-id")
-                console.log(updateId)
-                // this.view.updateEventHandeler(updateId)
+                let value =this.view.findUpdateValue(updateId)
+                if(value.title!=='' && value.startDate!=='' && value.endDate!==''){
+                    const newEvent = {eventName: value.title, startDate:value.startDate, endDate: value.endDate}
+                    this.model.updateEvent(updateId,newEvent).then(res =>this.view.updateRender(res))
+                }
+                else{
+                    //add alr
+                }
             }
         })
         
